5282fba4348804e098440f804ef68944463e77b7
Sagar Routh <sagarrouth0512@gmail.com>
2017-02-06 04:44:07 +0530

    Add the basic structure of app

---
+++ server.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+//open a connection to Mongo DB
+mongoose.connect("mongodb://localhost:27017/MEAN-TODO");
+
+//serve static files
+app.use(express.static(__dirname+'/public'));
+//logging during dev
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+//start server and listen on port 8080
+app.listen(8080, ()=>{console.log('server started');});
